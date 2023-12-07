2593f11e4431e0ef59899bae3241deb53c476a16
Odmund Bautista <odfelip.bautista@gmail.com>
2023-12-07 09:23:46 -0600

    Experimenting with colors

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Ready to be scripted!
+const lightSwitch = document.querySelector('.lightSwitch');
+
+let lightMode = true;
+let darkMode = false;
+lightSwitch.addEventListener('mouseup', function() {
+    if (!darkMode) {
+        document.body.style.background = '#C42348';
+        document.body.style.color = '#EF7674';
+        darkMode = true;
+        lightMode = false;
+    } else {
+        document.body.style.background = '#EF7674';
+        document.body.style.color = '#C42348';
+        lightMode = true;
+        darkMode = false;
+    }
+});
