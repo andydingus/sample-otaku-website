b2cda36a2837adb69c07d25cbc1f4e886b431e6c
Odmund Bautista <odfelip.bautista@gmail.com>
2023-12-08 15:42:24 -0600

    Style elements; experiment on layout

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 // Ready to be scripted!
-const lightSwitch = document.querySelector('.lightSwitch');
+const lightSwitch = document.querySelector('.btn');
 
 let lightMode = true;
 let darkMode = false;
 lightSwitch.addEventListener('mouseup', function() {
     if (!darkMode) {
-        document.body.style.background = '#C42348';
-        document.body.style.color = '#EF7674';
+        document.body.style.background = '#311D3F';
+        document.body.style.color = '#E23E57';
         darkMode = true;
         lightMode = false;
     } else {
-        document.body.style.background = '#EF7674';
-        document.body.style.color = '#C42348';
+        document.body.style.background = '#E23E57';
+        document.body.style.color = '#311D3F';
+        
         lightMode = true;
         darkMode = false;
     }
