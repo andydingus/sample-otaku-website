3e02241a0b1ea8fddd01243bc7fd1b633c7b4a1d
Odmund Bautista <odfelip.bautista@gmail.com>
2023-12-11 09:51:00 -0600

    Apply new color palette

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const lightSwitch = document.querySelector('.btn');
 
 let lightMode = true;
 let darkMode = false;
-lightSwitch.addEventListener('mouseup', function() {
-    if (!darkMode) {
-        document.body.style.background = '#311D3F';
-        document.body.style.color = '#E23E57';
-        darkMode = true;
-        lightMode = false;
-    } else {
-        document.body.style.background = '#E23E57';
-        document.body.style.color = '#311D3F';
+// lightSwitch.addEventListener('mouseup', function() {
+//     if (!darkMode) {
+//         document.body.style.background = '#311D3F';
+//         document.body.style.color = '#E23E57';
+//         darkMode = true;
+//         lightMode = false;
+//     } else {
+//         document.body.style.background = '#E23E57';
+//         document.body.style.color = '#311D3F';
         
-        lightMode = true;
-        darkMode = false;
-    }
-});
+//         lightMode = true;
+//         darkMode = false;
+//     }
+// });
