aae9b6110ac2afcf26b837f4b42cef85bb015af0
Odmund Bautista <odfelip.bautista@gmail.com>
2023-12-14 13:49:19 -0600

    Progress on light/dark mode functionality
    Currently, the navbar links won't change color even though it the rest
    of the page does. Look to fix that next

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 // Ready to be scripted!
 const lightSwitch = document.querySelector('.btn');
+const headerBanner = document.querySelector('h1.logo');
+let navLinks = document.getElementsByClassName('nav-link-text');
 let articles = document.getElementsByClassName('container article');
+
+
 
 let lightMode = true;
 let darkMode = false;
-// lightSwitch.addEventListener('mouseup', function() {
-//     if (!darkMode) {
-//         document.body.style.background = '#311D3F';
-//         document.body.style.color = '#E23E57';
-//         darkMode = true;
-//         lightMode = false;
-//     } else {
-//         document.body.style.background = '#E23E57';
-//         document.body.style.color = '#311D3F';
+lightSwitch.addEventListener('mouseup', function() {
+    if (!darkMode) {
+        document.body.style.background = '#311D3F';
+        document.body.style.color = '#F1ECE1';
+
+        // Applying the text color to the nav links
+        // !!Currently doesn't work!!
+        for (let i = 0; i < navLinks; i++) {
+            navLinks[i].style.color = 'inherit';
+        }
+
+        // Changing the banner
+        headerBanner.style.backgroundImage = 'url(images/blue.gif)';
+        headerBanner.style.backgroundBlendMode = 'normal';
+        darkMode = true;
+        lightMode = false;
+    } else {
+        document.body.style.background = '#E23E57';
+        document.body.style.color = '#311D3F';
         
-//         lightMode = true;
-//         darkMode = false;
-//     }
-// });
+        lightMode = true;
+        darkMode = false;
+    }
+});
 
 // Using the DOM and JS to add eventListeners to all class "container article" items
 // for (let i = 0; i < articles.length; i++) {
