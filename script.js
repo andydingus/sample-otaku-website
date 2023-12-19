bc7d0d41c33858fa2ec1ab05e2ca4f9e1c1cb5e1
Odmund Bautista <odfelip.bautista@gmail.com>
2023-12-19 16:52:37 -0600

    Setup for new feature
    It's quitting time! Details on what to accomplish are commented in code.

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ const navLinks = document.getElementsByClassName('nav-link-text');
 const articles = document.getElementsByClassName('container article');
 const articleLinks = document.getElementsByClassName('article-link');
 const buttons = document.getElementsByTagName('button');
+const trendingChapters = document.getElementsByClassName('chapter');
 
 let lightMode = true;
 let darkMode = false;
-lightSwitch.addEventListener('mouseup', function() {
+lightSwitch.addEventListener('mouseup', function () {
     if (!darkMode) {
         document.body.style.background = '#00506F';
         document.body.style.color = '#F1ECE1';
@@ -28,7 +29,7 @@ lightSwitch.addEventListener('mouseup', function() {
         headerBanner.style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.6)), url("images/blue.gif")';
         headerBanner.style.backgroundColor = 'rgba(0,0,0,0.3)';
         headerBanner.style.backgroundBlendMode = 'darken';
-        
+
         // Applying the text color to the article links
         for (let i = 0; i < articleLinks.length; i++) {
             articleLinks[i].style.color = '#FF6666';
@@ -61,7 +62,7 @@ lightSwitch.addEventListener('mouseup', function() {
         headerBanner.style.backgroundImage = 'linear-gradient(rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.7)), url("images/pink.gif")';
         headerBanner.style.backgroundColor = 'rgba(255,255,255,0.2)';
         headerBanner.style.backgroundBlendMode = 'lighten';
-        
+
         // Applying the text color to the article links
         for (let i = 0; i < articleLinks.length; i++) {
             articleLinks[i].style.color = '#AC7CB4';
@@ -78,6 +79,14 @@ lightSwitch.addEventListener('mouseup', function() {
         darkMode = false;
     }
 });
+
+// What is should do:
+// 1) On hover: any "covered" manga should be fully "shown" (change the width to what the top chapter has).
+// Top chapter should get "covered"
+// 2) After hover: top manga chapter becomes "uncovered", manga that was being hovered becomes "covered" again
+function addChapterSelect() {
+    
+}
 
 // Icon creds:
 // Streelight: <a href="https://www.flaticon.com/free-icons/street-light" title="street light icons">Street light icons created by Freepik - Flaticon</a>;
