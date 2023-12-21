0843e2db6a7e33fa4d3c974c652497a1e79c892d
Odmund Bautista <odfelip.bautista@gmail.com>
2023-12-21 09:52:39 -0600

    Add animations, small fixes
    +For the chapter and episodes section, the panels now do a start animation
    when they are visible in the viewport
    +Navbar links now properly transition into white when in dark mode

--- script.js
+++ script.js
@@ -9,6 +9,26 @@ const buttons = document.getElementsByTagName('button');
 const trendingChapters = document.getElementsByClassName('chapter');
 const trendingEpisodes = document.getElementsByClassName('episode');
 
+// IntersectionObserver elements
+// Allows for animation that plays when scrolled into view
+const inViewport = (entries, observer) => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+    });
+};
+
+const Obs = new IntersectionObserver(inViewport);
+const obsOptions = {
+    root: document.querySelector('.container.xtra-gap'),
+    threshold: 0.1,
+}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+
+// Attach observer to every [data-inviewport] element:
+document.querySelectorAll('[data-inviewport]').forEach(el => {
+  Obs.observe(el, obsOptions);
+});
+
+
 let lightMode = true;
 let darkMode = false;
 
@@ -24,27 +44,29 @@ function setEventListeners() {
             document.body.style.background = '#00506F';
             document.body.style.color = '#F1ECE1';
             document.body.style.transition = 'all 1s ease-out'; // Illusion of smooth 'transition'
-    
+
             // Changing nav icons
             for (let i = 0; i < navIcons.length; i++) {
                 navIcons[i].style.backgroundImage = 'url(images/streetlight.png)';
             }
-    
+
             // Applying the text color to the nav links
             for (let i = 0; i < navLinks.length; i++) {
                 navLinks[i].style.color = 'white';
+                navLinks[i].style.transition = 'all 1s ease-out';
             }
-    
+
             // Changing the banner
             headerBanner.style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.6)), url("images/blue.gif")';
             headerBanner.style.backgroundColor = 'rgba(0,0,0,0.3)';
             headerBanner.style.backgroundBlendMode = 'darken';
-    
+
             // Applying the text color to the article links
             for (let i = 0; i < articleLinks.length; i++) {
                 articleLinks[i].style.color = '#FF6666';
+                articleLinks[i].style.transition = 'all 1s ease-out';
             }
-    
+
             // Changing the buttons
             // this.textContent = 'Light Mode';
             this.setAttribute('src', 'images/moon.svg');
@@ -53,33 +75,33 @@ function setEventListeners() {
                 buttons[i].style.color = '#311D3F';
                 buttons[i].style.transition = 'all 1s ease-out'; // Animation
             }
-    
+
             darkMode = true;
             lightMode = false;
         } else {
             document.body.style.background = '#F1ECE1';
             document.body.style.color = '#311D3F';
-    
+
             // Changing nav icons
             for (let i = 0; i < navIcons.length; i++) {
                 navIcons[i].style.backgroundImage = 'url(images/sakura.png)';
             }
-    
+
             // Applying the text color to the nav links
             for (let i = 0; i < navLinks.length; i++) {
                 navLinks[i].style.color = '#311D3F';
             }
-    
+
             // Changing the banner
             headerBanner.style.backgroundImage = 'linear-gradient(rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.7)), url("images/pink.gif")';
             headerBanner.style.backgroundColor = 'rgba(255,255,255,0.2)';
             headerBanner.style.backgroundBlendMode = 'lighten';
-    
+
             // Applying the text color to the article links
             for (let i = 0; i < articleLinks.length; i++) {
                 articleLinks[i].style.color = '#AC7CB4';
             }
-    
+
             // Changing the lightswitch
             // this.textContent = 'Dark Mode';
             this.setAttribute('src', 'images/sun.svg');
@@ -88,7 +110,7 @@ function setEventListeners() {
                 buttons[i].style.color = '#F1ECE1';
                 buttons[i].style.transition = 'all 1s ease-out';
             }
-    
+
             lightMode = true;
             darkMode = false;
         }
@@ -98,7 +120,7 @@ function setEventListeners() {
         trendingChapters[i].addEventListener('mouseover', function () {
             trendingChapters[i].style.width = '400px';
         });
-    
+
         trendingChapters[i].addEventListener('mouseout', function () {
             trendingChapters[i].style.width = '100px';
         })
@@ -108,12 +130,14 @@ function setEventListeners() {
         trendingEpisodes[i].addEventListener('mouseover', function () {
             trendingEpisodes[i].style.width = '400px';
         });
-    
+
         trendingEpisodes[i].addEventListener('mouseout', function () {
             trendingEpisodes[i].style.width = '100px';
         })
     }
 }
+
+
 
 setEventListeners();
 
